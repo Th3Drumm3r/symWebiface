7b941cac70b377aba8485562b8e71c2e316311c9
Cédric <cedric@P8Z77.home>
2015-05-29 11:40:50 +0200

    Ajout du Bundle Home dans Content

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Account\UserBundle\AccountUserBundle(),
+            new Content\HomeBundle\ContentHomeBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Content/HomeBundle/ContentHomeBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Content\HomeBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ContentHomeBundle extends Bundle
+{
+}
